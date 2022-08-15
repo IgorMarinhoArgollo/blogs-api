f24fb7f42f5090327d2812b347751e0f0688a787
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-08-14 22:13:51 -0300

    feat: installing security meadures
    installing security measures as helmet, limit-rate, installing morgan logger, and error rescue

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const limiter = require('./middlewares/limiter');
 const error = require('./middlewares/error');
 
 const app = express();
+app.use(helmet());
+app.use(limiter);
 app.use(express.json());
+app.use(morgan('common'));
 
 app.use();
 
