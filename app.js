9f903263a5bc2fc6faf6c012dea740c817332611
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-08-14 22:19:55 -0300

    feat: installing swagger to create docs

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
 const limiter = require('./middlewares/limiter');
 const error = require('./middlewares/error');
+const swaggerDocs = require('./swagger.json');
 
 const app = express();
 app.use(helmet());
@@ -12,6 +14,8 @@ app.use(express.json());
 app.use(morgan('common'));
 
 app.use();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(error);
 
