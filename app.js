9129b6230dbe38525cb2de673e363f2b9b460faf
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-08-14 22:07:22 -0300

    feat: creating index, app and error middleware

---
+++ app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const error = require('./middlewares/error');
+
+const app = express();
+app.use(express.json());
+
+app.use();
+
+app.use(error);
+
+module.exports = app;
